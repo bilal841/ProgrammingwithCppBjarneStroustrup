9377181996fcc4b9efef508ac22d1546f493c476
Bilal <bilal841@gmail.com>
2022-06-23 12:57:39 +0500

    created chapter1Drill.cpp

---
+++ chapter1Drill.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+int main() //C++ programs starts by executing the main function
+{
+    cout<<"Hello, World!\n";  //output hello world
+    keep_window_open();     //wait for a character to be entered
+
+    return 0;
+}
