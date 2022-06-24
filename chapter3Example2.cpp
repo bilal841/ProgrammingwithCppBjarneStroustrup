ed966695a87c235914e02ab077685ac511a3a1f9
Bilal <bilal841@gmail.com>
2022-06-24 13:06:35 +0500

    updated chapter3Example2.cpp

---
+++ chapter3Example2.cpp
@@ -0,0 +1,27 @@
+
+//Variable Definition and initial values 
+#include "std_lib_facilities.h"
+
+int main()
+{
+    //variable decelaration and initialization
+    // type variable_name = variable_value;
+    // data_type variable_name = variable_value;
+
+    int number_of_steps = 39;
+    string name = "Annabel";
+
+    // Be sure to assign correct values to the variable according to their types
+
+    //int number_of_steps = "Bilal";
+    //string name1 = 30; 
+    
+    // Five most commonly used (data)types
+    int num_of_steps = 39;   // int for integers
+    double flying_time = 3.5;   // double for floating-point numbers
+    char decimal_point = '.';   // char for individual characters
+    string fullName = "Annemarie Schmeil";  // string for character strings
+    bool tap_on = true;         // bool for logical variables
+        
+    return 0;
+}
