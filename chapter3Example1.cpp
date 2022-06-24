4a850be44cea655bed74172c19c4da4715f9a788
Bilal <bilal841@gmail.com>
2022-06-24 12:42:03 +0500

    created Chapter3 Example 1 source code

---
+++ chapter3Example1.cpp
@@ -0,0 +1,10 @@
+//read and write first name
+#include "std_lib_facilities.h"
+int main()
+{
+    string first_name;
+    cout<<"Please enter your first name(followed by 'enter'):";
+    cin>>first_name;
+    cout<<"Hello,"<<first_name<<"!\n";
+    return 0 ;
+}
