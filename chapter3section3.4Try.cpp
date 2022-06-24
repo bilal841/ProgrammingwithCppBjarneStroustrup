715a1355dd770a4da36359287f3a1029e9f703a9
Bilal <bilal841@gmail.com>
2022-06-24 19:34:45 +0500

    section 3.4 Try section

---
+++ chapter3section3.4Try.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+int main()
+{
+    cout << "Please enter an integer value: ";
+    int n;
+    cin >> n;
+    double m;
+    m = n ;
+    cout<< "n == " << n
+        << "\nn+1 == " << n+1
+        << "\nthree times n == " << 3*n
+        << "\ntwice n == " << n+n
+        << "\nn squared == " << n*n
+        << "\nhalf of n == " << n/2
+        << "\nsquare root of n == " << sqrt(n)
+        << "\nsquare root of m == " << sqrt(m)
+        << '\n'; // name for newline (“end of line”) in output
+    return 0;
+}
