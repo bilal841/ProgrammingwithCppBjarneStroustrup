efa6740736e3789648676ae6cb7cdb56b10653e0
Bilal <bilal841@gmail.com>
2022-06-24 12:52:39 +0500

    updated chapter3Example1.cpp

--- chapter3Example1.cpp
+++ chapter3Example1.cpp
@@ -2,9 +2,22 @@
 #include "std_lib_facilities.h"
 int main()
 {
+    // first_name is a variable of type string
     string first_name;
+    
+    //Prompt to display on screen for the user
     cout<<"Please enter your first name(followed by 'enter'):";
+    
+    //read characters into the variable named first_name
     cin>>first_name;
-    cout<<"Hello,"<<first_name<<"!\n";
+    
+    //display the value of variable first_name
+    cout<<"Hello,"<<first_name<<"!\n"; 
+    
+    // The above line can also be written as follow but its needless repetition!
+    cout << "Hello, ";
+    cout << first_name;
+    cout << "!\n";
+    
     return 0 ;
 }
