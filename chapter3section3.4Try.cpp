fb732dcb10a8d7c6d8ecc9d5a07630e6640df678
Bilal <bilal841@gmail.com>
2022-06-26 09:37:47 +0500

    updated chapter3section3.4Try.cpp

--- chapter3section3.4Try.cpp
+++ chapter3section3.4Try.cpp
@@ -4,16 +4,17 @@ int main()
     cout << "Please enter an integer value: ";
     int n;
     cin >> n;
-    double m;
-    m = n ;
+    //double m;
+    //m = n ;
     cout<< "n == " << n
         << "\nn+1 == " << n+1
         << "\nthree times n == " << 3*n
         << "\ntwice n == " << n+n
         << "\nn squared == " << n*n
         << "\nhalf of n == " << n/2
-        << "\nsquare root of n == " << sqrt(n)
-        << "\nsquare root of m == " << sqrt(m)
+        << "\nsquare root of n - as int == " << sqrt(n)
+        //<< "\nsquare root of m == " << sqrt(m)
+        << "\nsquare root of n - as double == " << sqrt((double)n) // type conversion of int to double
         << '\n'; // name for newline (“end of line”) in output
     return 0;
 }
