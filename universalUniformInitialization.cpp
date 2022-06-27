5cf6f93806610b76f2f16901ab3938d5d42707c4
Bilal <bilal841@gmail.com>
2022-06-27 14:02:13 +0500

    completed chapter 3 examples

--- universalUniformInitialization.cpp
+++ universalUniformInitialization.cpp
@@ -5,5 +5,16 @@ int main()
     char b = a;
     cout<<"Integer == "<<a<<"\n";
     cout<<"Character == "<<(int)b<<"\n";
+
+    //outlaw narrowing conversion
+    double x {2.7}; //OK
+    //int y {x};      //error:double -> int might narrow
+
+    // When the initializer is an integer literal,
+    // compiler can check actual value and
+    // accept values that don't imply narrowing
+    
+    //char b1 {1000}; //error: narrowing assuming char is 1 byte long
+    char b4 {48}; // OK
     return 0;
 }
