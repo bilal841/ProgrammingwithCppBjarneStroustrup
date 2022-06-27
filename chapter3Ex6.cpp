abc3a488e09d367f0a6615915cd1b6681edd5556
Bilal <bilal841@gmail.com>
2022-06-27 19:13:48 +0500

    chap 3 ex 6

---
+++ chapter3Ex6.cpp
@@ -0,0 +1,13 @@
+//Arrange numbers entered by the user in a numerical sequence.
+#include "std_lib_facilities.h"
+int main()
+{
+    //3 Integers
+    int n1, n2, n3;
+
+    cout<<"Enter three integers(Press Enter after each input): ";
+    cin>>n1>>n2>>n3;
+
+    
+    return 0;
+}
