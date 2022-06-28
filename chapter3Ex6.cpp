fd4ed53ad1ec5e4cdc281066a4593a3df6076c50
Bilal <bilal841@gmail.com>
2022-06-28 08:55:20 +0500

    update chap 3 ex6

--- chapter3Ex6.cpp
+++ chapter3Ex6.cpp
@@ -5,9 +5,29 @@ int main()
     //3 Integers
     int n1, n2, n3;
 
-    cout<<"Enter three integers(Press Enter after each input): ";
+    cout<<"Enter three integers(Press Enter after each input)\n";
     cin>>n1>>n2>>n3;
 
-    
+    cout<<"You entered "<<n1<<" "<<n2<<" "<<n3<<"\n";
+
+    int largest=0, middle=0, smallest=0;
+
+    if(n1 > n2 && n2 > n3)
+    {
+        largest = n1;
+        middle = n2;
+        smallest = n3;
+    }
+    else if(n1 > n3 && n3 > n2)
+    {
+        largest = n1;
+        middle = n3;
+        smallest = n2;
+    }
+
+
+    cout<<"Largest "<<largest<<"\n";
+    cout<<"Middle "<<middle<<"\n";
+    cout<<"Smallest "<<smallest<<"\n";
     return 0;
 }
