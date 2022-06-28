84b1a80c7b9d3b84448e1bc1c5ad38ce2df7edd4
Bilal <bilal841@gmail.com>
2022-06-28 10:10:48 +0500

    completed Ch 3 Ex6

--- chapter3Ex6.cpp
+++ chapter3Ex6.cpp
@@ -11,23 +11,63 @@ int main()
     cout<<"You entered "<<n1<<" "<<n2<<" "<<n3<<"\n";
 
     int largest=0, middle=0, smallest=0;
+    //Check the largest first - then decide between the remaining two
+    // Larger of the remaining two will be the middle one .
 
-    if(n1 > n2 && n2 > n3)
+    //For the possibility that n1 is the largest
+    if(n1 >= n2 && n1 >= n3)
     {
         largest = n1;
-        middle = n2;
-        smallest = n3;
+        //For the possibility that n1 >= n2 >= n3
+        if(n2>=n3)
+        {
+            middle = n2;
+            smallest = n3;
+        }
+        //For the possibility that n1 >= n3 >= n2
+        else
+        {
+            middle = n3;
+            smallest=n2;
+        }
     }
-    else if(n1 > n3 && n3 > n2)
+    //For the possibility that n2 is the largest
+    else if(n2 >= n1 && n2 >= n3)
     {
-        largest = n1;
-        middle = n3;
-        smallest = n2;
+        largest = n2;
+        //For the possibility that n2 >= n1 >= n3
+        if(n1>=n3)
+        {
+            middle = n1;
+            smallest = n3;
+        }
+        //For the possibility that n2 >= n3 >= n1
+        else
+        {
+            middle = n3;
+            smallest=n1;
+        }
     }
-
-
+    //For the possibility that n3 is the largest
+    else if(n3 >= n1 && n3 >= n2)
+    {
+        largest = n3;
+        //For the possibility that n3 >= n1 >= n2
+        if(n1>=n2)
+        {
+            middle = n1;
+            smallest = n2;
+        }
+        //For the possibility that n3 >= n2 >= n1
+        else
+        {
+            middle = n2;
+            smallest=n1;
+        }
+    }
     cout<<"Largest "<<largest<<"\n";
     cout<<"Middle "<<middle<<"\n";
     cout<<"Smallest "<<smallest<<"\n";
+    cout<<largest<<" , "<<middle<<" , "<<smallest<<"\n";
     return 0;
 }
