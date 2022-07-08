4330ddba86270acb3ce377c7981a1dc1d603fc0c
Bilal <bilal841@gmail.com>
2022-07-08 10:41:55 +0500

    ex 10 - project for class

---
+++ Programming Principles and Practices using C++/code_and_exercise_solutions/chapter4Ex10.cpp
@@ -0,0 +1,38 @@
+/*
+    Author : Bilal Nasir
+    Chapter 4 Exercise 10 
+    
+    Problem Statement
+
+    Write a program that plays the game “Rock, Paper, Scissors.” If you are not
+    familiar with the game do some research (e.g., on the web using Google).
+    Research is a common task for programmers. Use a switch-statement to
+    solve this exercise. Also, the machine should give random answers (i.e.,
+    select the next rock, paper, or scissors randomly). Real randomness is too
+    hard to provide just now, so just build a vector with a sequence of values
+    to be used as “the next value.” If you build the vector into the program,
+    it will always play the same game, so maybe you should let the user enter some values.
+    Try variations to make it less easy for the user to guess
+    which move the machine will make next.
+
+    
+    Strategy:
+    Develop this game as a class project ?????
+    
+*/
+#include "std_lib_facilities.h"
+int main()
+{
+    try
+    {
+        // if(!cin)
+        // {
+        //     error("Something went wrong with the input \n");
+        // }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
+}
